782d4dc747c2d1e401fbc514684ff0d7992275e4
alice <alice.lezouret@ingesup.com>
2013-12-02 17:12:23 +0100

    ajout classe voiture

---
+++ agile-herma/Voiture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agile_herma
+{
+    class Voiture
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        //public int vitesse { get; set; }
+
+
+    }
+}
