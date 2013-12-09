f6ce08b469030ec4bedb613f143559ff4156a08e
Quentin <ferrerquentin88@gmail.com>
2013-12-09 14:21:31 +0100

    LastQVersion

--- agile-herma/Voiture.cs
+++ agile-herma/Voiture.cs
@@ -6,12 +6,46 @@ using System.Threading.Tasks;
 
 namespace agile_herma
 {
-    class Voiture
-    {
-        public int x { get; set; }
-        public int y { get; set; }
-        //public int vitesse { get; set; }
+   public class Voiture
+   {
+       private int _x;
+       private int _y;
 
 
-    }
+       //public int vitesse { get; set; }
+
+
+       public int X
+       {
+           get { return _x; }
+           set { _x = value; }
+       }
+
+       public int Y
+       {
+           get { return _y; }
+           set { _y = value; }
+       }
+
+
+       public Voiture(int pX,int pY)
+       {
+           this.X = pX;
+           this.Y = pY;
+       }
+
+
+       public Voiture()
+       {
+           this.X = 0;
+           this.Y = 0;
+       }
+
+       public String positionParDefaut()
+       {
+           String coord = "La voiture est a : " + X + ", " + Y;
+           return coord;
+       }
+
+   }
 }
